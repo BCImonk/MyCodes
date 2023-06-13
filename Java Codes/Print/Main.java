8284a61319a6b67a37392f2491bd23f498446a98
Harshit Singh <bciandcoding@gmail.com>
2023-06-13 12:27:39 +0530

    Add files via upload

---
+++ Java Codes/Print/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name:");
+        String name = sc.nextLine();
+        System.out.print("Reg No:");
+        String Regno = sc.nextLine();
+        System.out.print("Branch:");
+        String Branch = sc.nextLine();
+        System.out.print("year of passing:");
+        String pass = sc.nextLine();
+    }
+}
